f83a3208e19ceec1b00cc4c079c906ffc7bfed1b
Sergei Brykalov <106953297+Sebryk@users.noreply.github.com>
2023-12-21 21:56:10 +0300

    First commit

---
+++ dist/index.c01ddaac.js
@@ -0,0 +1,49 @@
+const typedTextSpan = document.querySelector(".typed-text");
+const cursorSpan = document.querySelector(".cursor");
+const textArray = [
+    "Sergei Brykalov"
+];
+const typingDelay = 200;
+const erasingDelay = 100;
+const newTextDelay = 5000;
+let textArrayIndex = 0;
+let charIndex = 0;
+function type() {
+    if (charIndex < textArray[textArrayIndex].length) {
+        if (!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+        typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
+        charIndex++;
+        setTimeout(type, typingDelay);
+    } else {
+        cursorSpan.classList.remove("typing");
+        setTimeout(erase, newTextDelay);
+    }
+}
+function erase() {
+    if (charIndex > 0) {
+        if (!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+        typedTextSpan.textContent = textArray[textArrayIndex].substring(0, charIndex - 1);
+        charIndex--;
+        setTimeout(erase, erasingDelay);
+    } else {
+        cursorSpan.classList.remove("typing");
+        textArrayIndex++;
+        if (textArrayIndex >= textArray.length) textArrayIndex = 0;
+        setTimeout(type, typingDelay + 2700);
+    }
+}
+document.addEventListener("DOMContentLoaded", function() {
+    if (textArray.length) setTimeout(type, 300);
+});
+const swiper = new Swiper(".swiper", {
+    loop: false,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev"
+    }
+});
+
